b0a3b3f68fb73669c53f74521166b6c5d51f1501
Kirk <kirk.woll@gmail.com>
2019-06-08 13:27:09 -0700

    Before converting fody package to .net standard

---
+++ SoMeta.Fody/ModuleWeaver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Fody;
+
+namespace SoMeta.Fody
+{
+    public class ModuleWeaver : BaseModuleWeaver
+    {
+        public override IEnumerable<string> GetAssembliesForScanning()
+        {
+            return new[] {"netstandard", "mscorlib"};
+        }
+
+        public override void Execute()
+        {
+            LogError("Test");
+        }
+    }
+}
---
+++ SoMeta.Helpers/SoMetaPropertyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SoMeta.Helpers
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public abstract class SoMetaPropertyAttribute : Attribute
+    {
+        protected virtual object GetPropertyValue(object instance, object currentValue)
+        {
+            return currentValue;
+        }
+
+        protected virtual void SetPropertyValue(object instance, object newValue, Action<object> proceed)
+        {
+            proceed(newValue);
+        }
+    }
+}
