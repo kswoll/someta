18f3f0cfd22c912dde12fdb9b2352c78152f8d25
Kirk Woll <kirk.woll@gmail.com>
2022-04-21 10:17:15 -0700

    add non public access docs

---
+++ Someta.Docs/Samples/NonPublicAccessExample.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Someta.Docs.Samples;
+
+[TestFixture]
+public class NonPublicAccessExample
+{
+    [Test]
+    #region NonPublicAccessExample
+    public void NonPublicAccess()
+    {
+        var testClass = new NonPublicAccessTestClass();
+        int counter = 0;
+        testClass.PropertyChanged += (sender, args) => counter++;
+        testClass.Value = 42;
+        Console.WriteLine(counter);         // Prints 1
+    }
+
+    [NotifyPropertyChanged]
+    class NonPublicAccessTestClass : INotifyPropertyChanged
+    {
+        // Except for this property, the rest of this class would be suitable as a base class for any class that wants to reuse this logic.
+        public int Value { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [InjectTarget(nameof(OnPropertyChanged))]
+        protected virtual void OnPropertyChanged(string propertyName, object oldValue, object newValue)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // this is only here to cause an overload situation
+        protected virtual void OnPropertyChanged(string propertyName, object oldValue, string newValue)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class NotifyPropertyChanged : Attribute, IPropertySetInterceptor, INonPublicAccess
+    {
+        [InjectAccess("OnPropertyChanged")]
+        public Action<object, string, object, object>? OnPropertyChanged { get; set; }
+
+        public void SetPropertyValue(PropertyInfo propertyInfo, object instance, object oldValue, object newValue, Action<object> setter)
+        {
+            if (!Equals(oldValue, newValue))
+            {
+                setter(newValue);
+                OnPropertyChanged?.Invoke(instance, propertyInfo.Name, oldValue, newValue);
+            }
+        }
+    }
+    #endregion
+}
